159ace7b783fb26de0f96be166da6d5cdd416d17
jieyixin <2027921697@qq.com>
2022-11-09 23:42:34 +0800

    modify readme

--- make_dataset.py
+++ make_dataset.py
@@ -7,7 +7,38 @@ tokenizer_path = "cache/vocab_small.txt"
 full_tokenizer = tokenization_bert.BertTokenizer(vocab_file=tokenizer_path)
 all_ids = []
 
-with open('D:\\VSCode\\GPT2-Chinese\\data\\train.txt', 'r', encoding='utf8') as f:
+# with open('D:\\VSCode\\GPT2-Chinese\\data\\train.txt', 'r', encoding='utf8') as f:
+#     for line in tqdm(f):
+#         l = line.strip()
+#         token_l = full_tokenizer.convert_tokens_to_ids(full_tokenizer.tokenize(l))
+#         if len(token_l) >= 20 and len(token_l) <= 1024:
+#             pad_len = 1024 - len(token_l)
+#             token_l.extend([0] * pad_len)
+#             all_ids.append(token_l)
+
+# with open('D:\\VSCode\\GPT2-Chinese\\data\\train_tok.txt', 'w', encoding='utf8') as f:
+#     for ids in all_ids:
+#         for id in ids:
+#             f.write(str(id) + ' ')
+#         f.write('\n')
+
+# with open('D:\\VSCode\\GPT2-Chinese\\data\\valid.txt', 'r', encoding='utf8') as f:
+#     for line in tqdm(f):
+#         l = line.strip()
+#         token_l = full_tokenizer.convert_tokens_to_ids(full_tokenizer.tokenize(l))
+#         if len(token_l) >= 20 and len(token_l) <= 1024:
+#             pad_len = 1024 - len(token_l)
+#             token_l.extend([0] * pad_len)
+#             all_ids.append(token_l)
+
+# with open('D:\\VSCode\\GPT2-Chinese\\data\\valid_tok.txt', 'w', encoding='utf8') as f:
+#     for ids in all_ids:
+#         for id in ids:
+#             f.write(str(id) + ' ')
+#         f.write('\n')
+
+
+with open('D:\\VSCode\\GPT2-Chinese\\data\\test.txt', 'r', encoding='utf8') as f:
     for line in tqdm(f):
         l = line.strip()
         token_l = full_tokenizer.convert_tokens_to_ids(full_tokenizer.tokenize(l))
@@ -16,7 +47,7 @@ with open('D:\\VSCode\\GPT2-Chinese\\data\\train.txt', 'r', encoding='utf8') as 
             token_l.extend([0] * pad_len)
             all_ids.append(token_l)
 
-with open('D:\\VSCode\\GPT2-Chinese\\data\\train_tok.txt', 'w', encoding='utf8') as f:
+with open('D:\\VSCode\\GPT2-Chinese\\data\\test_tok.txt', 'w', encoding='utf8') as f:
     for ids in all_ids:
         for id in ids:
             f.write(str(id) + ' ')
